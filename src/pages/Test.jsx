ea48de2aef1c222ec2613832d0171d9b507f6cb6
Nickolas499 <ezequielalmonte499@gmail.com>
2023-07-14 16:05:07 -0400

    07/14/2023

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,8 +1,37 @@
-
+import { user_data } from "../data/kpi_data";
 
 export const Test = () => {  
   return (
-    <div>text</div>
+    <div className="Test_container">
+      <ul className="Table_Asigment">
+        <li className="li_header">
+          <span className="span_value">Asigment</span>
+          <span className="span_value">LS3</span>
+          <span className="span_value">Zeiss</span>
+          <span className="span_value">3Shape</span>
+          <span className="span_value">Dessign IBO</span>
+          <span className="span_value">Digital Abut</span>
+          <span className="span_value">Phisycal Abut</span>
+          <span className="span_value">Full Arch</span>
+          <span className="span_value"></span>
+
+        </li>
+        {user_data.map((card, index) => (
+          <li className="li_item" key={index}>
+          <span className="span_value">{card.name}</span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><input type="checkbox" /></span>
+          <span className="span_value"><button className="btn">Asign</button></span>
+        </li>          
+        ))}
+        
+      </ul>
+    </div>
   )
 };
 
